e62e83cba76b3cf43893b41518150d89aaebf273
Douglas Urner <dlu@canishe.com>
2023-01-18 11:45:43 -0800

    Add comment

--- Prototype-4/Assets/Scripts/WebglPreBuildProcessing.cs
+++ Prototype-4/Assets/Scripts/WebglPreBuildProcessing.cs
@@ -1,4 +1,12 @@
 #if UNITY_EDITOR
+
+/*
+** Set up the WebGL build environment for Unity < 2021.3.41f1 on
+** macOS versions later than 12.3 (Monteray) where Apple has removed
+** the system version of Python. Add this script and install Python 3,
+** adjust the path to match where you have Python 3 installed.
+*/
+
 using UnityEditor.Build;
 using UnityEditor.Build.Reporting;
 
